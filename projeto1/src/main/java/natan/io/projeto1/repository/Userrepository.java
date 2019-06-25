9fe108987578031a240903c28610e3361426ccce
AlvaroPereiradoNascimento <alvaropereira15@gmail.com>
2019-06-25 09:55:08 -0300

    removido pastas desnecessárias
    removida pastas desncessárias para o funcionamento do sistema.

--- projeto1/src/main/java/natan/io/projeto1/repository/Userrepository.java
+++
@@ -1,9 +0,0 @@
-package natan.io.projeto1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import natan.io.projeto1.entity.User;
-
-public interface Userrepository extends JpaRepository<User, Long> {
-
-}

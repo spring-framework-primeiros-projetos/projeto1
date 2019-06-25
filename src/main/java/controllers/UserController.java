2aee355b62b6736b96aacb4e2a3a2db17bf8aa06
AlvaroPereiradoNascimento <alvaropereira15@gmail.com>
2019-06-25 09:51:52 -0300

    Criado Classe Controller
    Gerado Classe controller para  envio de documentos ao front
    Utilizado @RequestMapping pois ele deteminará a rota efetuada para que o front possa encontrar o seu controller.

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import natam.io.Projeto1.entity.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Alvaro
+ */
+@Controller
+public class UserController {
+    private UserRepository userRepository;
+    
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+        
+    }
+    @RequestMapping("/user")
+    public String getUsers (Model model){
+      model.addAttribute("usersList", userRepository.findAll());
+      return "user";
+        
+    }
+    
+    
+}
